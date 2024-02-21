385ae98f29631a3c33df4c88b4e6a65c95eb22e0
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-02-21 15:21:00 +0100

    add AddToCart button component

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const AddToCart = () => {
+
+return (
+    <>
+    <button type="button">Añadir al carrito </button>
+    </>
+)
+
+}
+
+export default AddToCart 
--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import AddToCart from "./AddToCart.jsx"
 
 const CardDetail = () => {
     return (
     <>
-    
+    <div className="detailCard">
+        <h1>Titulo mueble</h1>
+        <p>Descripción</p>
+        <div>
+        <img src="" alt="" />
+        <p>Precio</p>
+        </div>
+    </div>
+    <AddToCart/>
     </>
     )
 }
